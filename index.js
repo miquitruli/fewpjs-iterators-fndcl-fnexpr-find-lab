95f0dcc087bd37d0db9a110806ca43fcddb68ed4
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-06 17:00:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+function superbowlWin(array) {
+  let result = array.find(array => array.result === 'W')
+  if(result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+}
